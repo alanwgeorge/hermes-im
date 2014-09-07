a814dbef6f923dbe0867fc44a644a29dda96152c
alan.wesley.george@gmail.com <alan.wesley.george@gmail.com@7ef383bf-5cfa-a659-297f-882c07cf9905>
2014-09-07 17:27:15 +0000

    Sync first parts of password

--- app/src/main/java/com/alangeorge/android/hermes/SignOnFragment.java
+++ app/src/main/java/com/alangeorge/android/hermes/SignOnFragment.java
@@ -2,16 +2,20 @@ package com.alangeorge.android.hermes;
 
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
-/**
- *
- */
 public class SignOnFragment extends Fragment {
+    private static final String TAG = "SignOnFragment";
+
+    private TextView passwordView;
 
     public SignOnFragment() {
         // Required empty public constructor
@@ -20,15 +24,26 @@ public class SignOnFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        @SuppressWarnings("UnnecessaryLocalVariable") View view = inflater.inflate(R.layout.fragment_sign_on, container, false);
+        @SuppressWarnings("UnnecessaryLocalVariable")
+        View view = inflater.inflate(R.layout.fragment_sign_on, container, false);
+
+        passwordView = (TextView) view.findViewById(R.id.editText_password);
 
         return view;
     }
 
-
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    public void signOn(View view) {
+        Log.d(TAG, "signOn(" + view + ")");
+        Log.d(TAG, "password entered: " + passwordView.getText().toString());
+
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(passwordView.getWindowToken(), 0);
+
+        passwordView.setText("");
     }
 }
